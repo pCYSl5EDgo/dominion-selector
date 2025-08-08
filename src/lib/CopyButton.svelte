7630a2571b4c798ecc34e02d6cb85d5a130fe520
pcysl5edgo <31692496+pCYSl5EDgo@users.noreply.github.com>
2025-08-08 20:47:27 +0900

    fix navigator

--- src/lib/CopyButton.svelte
+++ src/lib/CopyButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
  import { type Snippet } from "svelte";
  let { text, fadeoutMilliseconds = 1000, children }: { text: string; fadeoutMilliseconds?: number; children: Snippet } = $props();
- const resultText = navigator?.clipboard == null ? "コピー失敗" : "コピー完了";
+ const resultText = self?.navigator?.clipboard == null ? "コピー失敗" : "コピー完了";
  let dom: HTMLSpanElement;
  function onclick() {
   dom.classList.remove("hidden");
